81425e4d9d97c71fabf14a2e1f179219043ab5ce
Mahmoud Bakr <mbakr6821@gmail.com>
2021-09-12 19:46:34 +0200

    Combine reducers

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
 import userReducer from './user';
+import measureReducer from './measure';
 
-export default combineReducers({ userReducer });
+export default combineReducers({ userReducer, measureReducer });
