6753d1814f8798a31f998d1680a07f64b3268936
Mahmoud Bakr <mbakr6821@gmail.com>
2021-10-09 23:07:06 +0200

    Removing console

--- src/Components/TrainingForm.js
+++ src/Components/TrainingForm.js
@@ -20,7 +20,6 @@ const TrainingForm = () => {
     };
     const response = await fetch(trainingURL, config);
     const data = await response.json();
-    console.log(data);
     history.push('/all/trainings');
   };
 
